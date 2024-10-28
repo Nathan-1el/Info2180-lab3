fbd535a40f8db55bcc92c46f72fddfffd9b21081
Nathaniel s <nathaniel.student1@gmail.com>
2024-10-27 21:27:29 -0500

    added exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
-   
     const squares = document.querySelectorAll("#board > div");
-  
+    const gameState = Array(9).fill(null);
+    let currentPlayer = "X"; 
+
     
     squares.forEach(square => {
       square.classList.add("square");
     });
-  });
 
+    
+    squares.forEach((square, index) => {
 
+      
+      square.addEventListener("click", function() {
 
-  
+        if (!square.textContent) {
+          square.textContent = currentPlayer;
+          square.classList.add(currentPlayer);
+
+          gameState[index] = currentPlayer;
+
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
+      });
+
+      
+      square.addEventListener("mouseenter", function() {
+        if (!square.textContent) {
+            square.classList.add("hover");
+        }
+      });
+
+      
+      square.addEventListener("mouseleave", function() {
+          square.classList.remove("hover");
+      });
+    });
+    
+});
